948c2fb0cc9b680e9f7517d8c40474aa920b9326
Roman <roman.ryzhenko66@gmail.com>
2025-02-15 20:52:56 +0300

    copy animation

--- src/components/ContributorsTable/columns.tsx
+++ src/components/ContributorsTable/columns.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 import { type ColumnDef } from "@tanstack/react-table";
 import Image from "next/image";
 import { Button } from "../ui/button";
+import { useSpring, animated } from "@react-spring/web";
+import { useState } from "react";
 
 export type Contributor = {
   name: string;
@@ -58,9 +61,63 @@ export const columns: ColumnDef<Contributor>[] = [
     accessorKey: "address",
     header: "Address",
     size: 353,
-    cell: ({ cell }) => (
-      <p className="text-[#ABBDCC]">{cell.getValue() as string}</p>
-    ),
+    cell: ({ cell }) => {
+      const [clicked, setClicked] = useState(false);
+      const [isHovering, setIsHovering] = useState(false);
+
+      const handleMouseEnter = () => {
+        setIsHovering(true);
+      };
+
+      const handleMouseLeave = () => {
+        setIsHovering(false);
+      };
+
+      const copyProps = useSpring({
+        opacity: clicked ? 0 : 1,
+        right: clicked ? 30 : 20,
+      });
+      const checkProps = useSpring({
+        opacity: clicked ? 1 : 0,
+        right: clicked ? 20 : 30,
+      });
+
+      return (
+        <p
+          onClick={() => {
+            if (!clicked) {
+              setClicked(true);
+              setTimeout(() => setClicked(false), 3000);
+              void navigator.clipboard.writeText(cell.getValue() as string)
+            }
+          }}
+          className="relative flex w-[151px] cursor-pointer items-center gap-2"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          style={{
+            color: clicked ? "#4DA2FF" : isHovering ? "white" : "#ABBDCC",
+          }}
+        >
+          {cell.getValue() as string}
+          <animated.div className="absolute" style={copyProps}>
+            <Image
+              src={"/images/squares.png"}
+              alt="squares"
+              width={16}
+              height={16}
+            />
+          </animated.div>
+          <animated.div className="absolute" style={checkProps}>
+            <Image
+              src={"/images/checkmark.png"}
+              alt="checkmark"
+              width={16}
+              height={16}
+            />
+          </animated.div>
+        </p>
+      );
+    },
   },
   {
     accessorKey: "stampsCollected",
@@ -72,8 +129,8 @@ export const columns: ColumnDef<Contributor>[] = [
     header: "Points",
     size: 90,
     cell: ({ cell }) => (
-      <div className="flex gap-6 items-center relative">
-        <span className="flex gap-2 absolute">
+      <div className="relative flex items-center gap-6">
+        <span className="absolute flex gap-2">
           <Image
             src={"/images/drop.png"}
             alt="drop-icon"
@@ -84,7 +141,7 @@ export const columns: ColumnDef<Contributor>[] = [
         </span>
         <Button
           variant="secondary"
-          className="absolute h-[44px] w-[109px] items-center gap-2 hidden right-0"
+          className="absolute right-0 hidden h-[44px] w-[109px] items-center gap-2"
         >
           Details
           <Image
