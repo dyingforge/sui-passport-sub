cfeeed4ae79f7b33fe30bce8f6a52d1dc465bc52
Wulabalabo <cyberchenjw@gmail.com>
2025-02-24 13:15:42 +0800

    [fix] set stashed only mainnet

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,25 +7,21 @@ import { networkConfig, network } from "~/lib/contracts"
 import "@mysten/dapp-kit/dist/index.css";
 import { UserProfileProvider } from "~/context/user-profile-context";
 import { PassportsStampsProvider } from "~/context/passports-stamps-context";
-import { registerStashedWallet } from '@mysten/zksend';
-import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            registerStashedWallet('Sui Passport', {
-                network: network,
-            });
-        }
-    }, []);
     return (
         <QueryClientProvider client={queryClient}>
             <SuiClientProvider networks={networkConfig} defaultNetwork={network}>
                 <PassportsStampsProvider>
                     <UserProfileProvider>
-                        <WalletProvider>
+                        <WalletProvider autoConnect stashedWallet={
+                            {
+                                name: "Sui Passport",
+                                network: "mainnet",
+                            }
+                        }>
                             {children}
                         </WalletProvider>
                     </UserProfileProvider>
