1dc627fa525a9d4fc6e5087b114d2efeaa97180f
Wulabalabo <cyberchenjw@gmail.com>
2025-02-26 03:00:56 +0800

    [fix] next.config.js

--- next.config.js
+++ next.config.js
@@ -12,36 +12,7 @@ const config = {
                 hostname: "images.suipassport.app",
             },
         ],
-    },
-    async headers() {
-      return [
-          {
-              source: '/:path*',
-              headers: [
-                  {
-                      key: 'referrer-policy',
-                      value: 'no-referrer-when-downgrade',  // 最宽松的 referrer policy
-                  },
-                  {
-                      key: 'Cross-Origin-Opener-Policy',
-                      value: 'unsafe-none',  // 最宽松的 COOP
-                  },
-                  {
-                      key: 'Cross-Origin-Embedder-Policy',
-                      value: 'unsafe-none',  // 最宽松的 COEP
-                  },
-                  {
-                      key: 'Cross-Origin-Resource-Policy',
-                      value: 'cross-origin'  // 最宽松的 CORP
-                  },
-                  {
-                      key: 'Access-Control-Allow-Origin',
-                      value: '*'  // 允许所有域名访问
-                  }
-              ],
-          },
-      ];
-  },
+    }
 };
 
 export default config;
