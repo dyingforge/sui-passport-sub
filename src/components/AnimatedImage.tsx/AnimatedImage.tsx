f6815669da6aa0f6425db1c0b4af6b98b9f6b4f9
Mindfrog <cyberchenjw@gmail.com>
2025-05-01 13:29:41 +0800

    [fix] try to update image

--- src/components/AnimatedImage.tsx/AnimatedImage.tsx
+++ src/components/AnimatedImage.tsx/AnimatedImage.tsx
@@ -53,7 +53,7 @@ export const AnimatedImage: FC<Props> = (props) => {
         rotateZ,
       }}
     >
-      <Image {...props} />
+      <img {...props} alt={props.alt ?? ""} />
     </animated.div>
   );
 };
